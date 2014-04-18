722fd43ba2502f0d8d56d8c4fa46b9598deafec7
aspin <kevinchen@u.northwestern.edu>
2014-04-18 08:02:33 +0100

    update icon

--- gen/com/aspin/prototyper/R.java
+++ gen/com/aspin/prototyper/R.java
@@ -33,6 +33,7 @@ public final class R {
         public static final int ic_quit=0x7f020002;
         public static final int ic_start=0x7f020003;
         public static final int logo2=0x7f020004;
+        public static final int logo3=0x7f020005;
     }
     public static final class id {
         public static final int action_settings=0x7f0a0006;
